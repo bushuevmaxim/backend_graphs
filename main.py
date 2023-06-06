ffc43977f7a75c84fc88fa40292497358704c331
Max Bushuev <bushuevmaxim55@gmail.com>
2023-06-06 11:59:18 +0600

    3

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ matplotlib.use('agg')
 path_fig = "fig.png"
 path_paint = "paint_gif.png"
 @app.post("/show_graph")
-def show_graph(graph: List[List[int]]):
+async def show_graph(graph: List[List[int]]):
     G = nx.Graph(np.matrix(graph), create_using=nx.Graph)
     nx.draw_circular(G, node_color='brown',font_color = "whitesmoke", node_size=1000,font_size=22, with_labels=True)
     plt.savefig(path_fig, format="png")
@@ -37,19 +37,8 @@ def show_graph(graph: List[List[int]]):
     data = {"img": b64_string}
     json_data = jsonable_encoder(data)
     return JSONResponse(content=json_data)
-@app.post("/test")
-def test(graph: List[List[int]]):
-    G = nx.Graph(np.matrix(graph), create_using=nx.Graph)
-    nx.draw_circular(G, node_color='brown',font_color = "whitesmoke", node_size=1000,font_size=22, with_labels=True)
-    plt.savefig(path_fig, format="PNG")
-    plt.clf()
-    with open(path_fig, "rb") as img_file:
-        b64_string = base64.b64encode(img_file.read())
-    data = {"img": b64_string, "count": {4}}
-    json_data = jsonable_encoder(data)
-    return JSONResponse(content=json_data)
 @app.post("/paint_graph")
-def paint_graph(graph: List[List[int]], temperature: float, cooling_rate: float):
+async def paint_graph(graph: List[List[int]], temperature: float, cooling_rate: float):
     G = nx.Graph(np.matrix(graph), create_using=nx.Graph)
     colorlist = list(paint_graph(graph,temperature, cooling_rate ))
     nx.draw_circular(G, node_color=colorlist,font_color = "black", node_size=1000,font_size=22, with_labels=True)
@@ -70,35 +59,42 @@ def simulated_annealing(adjacency_matrix, initial_temperature, cooling_rate):
     num_nodes = len(adjacency_matrix)
     current_solution = initialize_solution(num_nodes)  
     current_cost = calculate_cost(adjacency_matrix, current_solution) 
+    best_solution =current_solution.copy()
     temperature = initial_temperature
-
     while temperature > 0.1:  
-        for i in range(100): 
-            new_solution = generate_neighbor_solution(current_solution)
-            new_cost = calculate_cost(adjacency_matrix, new_solution) 
+            for i in range(100):
+                new_solution = generate_neighbor_solution(current_solution)
+                
+                new_cost = calculate_cost(adjacency_matrix, new_solution )
+                if new_cost < current_cost  :
+                    current_solution = new_solution
+                    current_cost = new_cost
+                
+                    if new_cost < calculate_cost(adjacency_matrix, best_solution):
+                        best_solution = new_solution
+                    
+                else:
+                    probability = math.exp((current_cost - new_cost) / temperature)
+                    if random.random() < probability:
+                        current_solution = new_solution
 
-            if new_cost < current_cost:
-                current_solution = new_solution
-                current_cost = new_cost
-            num_colors_used = len(set(current_solution))
-            if num_colors_used == num_nodes and current_cost == 0:
-                return current_solution
-        temperature *= cooling_rate
+            temperature -= cooling_rate
 
-    return current_solution
+    return best_solution
 
 def initialize_solution(num_nodes):
     solution = [-1] * num_nodes
     return solution
 
 def calculate_cost(adjacency_matrix, solution):
-    cost = 0
+    cost = 1
     num_nodes = len(adjacency_matrix)
+    num_colors_used = len(set(solution))
     for i in range(num_nodes):
-        for j in range(i + 1, num_nodes):
+        for j in range(i+1, num_nodes):
             if adjacency_matrix[i][j] == 1 and solution[i] == solution[j]:
                 cost += 1
-    return cost
+    return cost * num_colors_used
 
 def generate_neighbor_solution(solution):
     new_solution = solution.copy()
